39bda9b4c561a365ff7599a5576ae08ae0e46896
gukai <307475423@qq.com>
2016-04-25 23:02:43 +0800

    支持redis的额外参数，添加淘宝ruby源，确保下载顺利.

--- redis-cluster/entrypoint.sh
+++ redis-cluster/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 if [[ ${redis_type} == "local" ]]; then
-    redis-server /usr/local/redis.conf
+    redis-server /usr/local/redis.conf $1
 elif [[ ${redis_type} == "master" ]]; then
     redis-server /usr/local/redis.conf
 elif [[ ${redis_type} == "slave" ]]; then
     if [[ ${GET_HOSTS_FROM:-dns} == "env" ]]; then
-        redis-server /usr/local/redis.conf --slaveof ${REDIS_MASTER_SERVICE_HOST} 6379
+        redis-server /usr/local/redis.conf --slaveof ${REDIS_MASTER_SERVICE_HOST} 6379 $1
     else
-        redis-server /usr/local/redis.conf --slaveof redis-master 6379
+        redis-server /usr/local/redis.conf --slaveof redis-master 6379 $1
     fi
 elif [[ ${redis_type} == "cluster" ]]; then
-    redis-server /usr/local/redis_cluster.conf
+    redis-server /usr/local/redis_cluster.conf $1
 else
     redis-server $1
 fi
